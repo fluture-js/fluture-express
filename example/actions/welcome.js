cbddff0913fc94c772bf61cbe6bf128668ea121b
Jan-Vidar Bakke <janvidar@fink.no>
2020-08-21 08:43:26 +0200

    Update example/README

--- example/actions/welcome.js
+++ example/actions/welcome.js
@@ -3,5 +3,5 @@ const Future = require ('fluture');
 
 module.exports = (req, locals) => {
   const user = locals.session.id ? `user ${locals.session.id}` : 'stranger';
-  return Future.resolve (Render (200) ('index') ({user}))
+  return Future.resolve (Render (200) ('index') ({user}));
 };
--- example/actions/welcomeJson.js
+++ example/actions/welcomeJson.js
@@ -3,5 +3,5 @@ const Future = require ('fluture');
 
 module.exports = (req, locals) => {
   const welcome = locals.session.id ? `user ${locals.session.id}` : 'stranger';
-  Future.resolve (Json (200) ({welcome}));
+  return Future.resolve (Json (200) ({welcome}));
 };
